bf720e75453e988b0a1599f08a8c825c9facf35b
Илья <ilel@list.ru>
2020-02-28 09:38:47 +0700

    Добавьте файлы проекта.

---
+++ ExcelToBitmap/ExcelToBitmap.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ExcelToBitmap
+{
+    partial class ExcelToBitmap
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblExcel = new System.Windows.Forms.Label();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.txtExcel = new System.Windows.Forms.TextBox();
+            this.txtPathOut = new System.Windows.Forms.TextBox();
+            this.btnAddPathOut = new System.Windows.Forms.Button();
+            this.lblPathOut = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblExcel
+            // 
+            this.lblExcel.AutoSize = true;
+            this.lblExcel.Location = new System.Drawing.Point(64, 15);
+            this.lblExcel.Name = "lblExcel";
+            this.lblExcel.Size = new System.Drawing.Size(68, 13);
+            this.lblExcel.TabIndex = 0;
+            this.lblExcel.Text = "Файл Excel:";
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(494, 12);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(20, 20);
+            this.btnExcel.TabIndex = 1;
+            this.btnExcel.Text = "+";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // txtExcel
+            // 
+            this.txtExcel.Location = new System.Drawing.Point(138, 12);
+            this.txtExcel.Name = "txtExcel";
+            this.txtExcel.ReadOnly = true;
+            this.txtExcel.Size = new System.Drawing.Size(350, 20);
+            this.txtExcel.TabIndex = 2;
+            // 
+            // txtPathOut
+            // 
+            this.txtPathOut.Location = new System.Drawing.Point(138, 38);
+            this.txtPathOut.Name = "txtPathOut";
+            this.txtPathOut.ReadOnly = true;
+            this.txtPathOut.Size = new System.Drawing.Size(350, 20);
+            this.txtPathOut.TabIndex = 5;
+            // 
+            // btnAddPathOut
+            // 
+            this.btnAddPathOut.Location = new System.Drawing.Point(494, 38);
+            this.btnAddPathOut.Name = "btnAddPathOut";
+            this.btnAddPathOut.Size = new System.Drawing.Size(20, 20);
+            this.btnAddPathOut.TabIndex = 4;
+            this.btnAddPathOut.Text = "+";
+            this.btnAddPathOut.UseVisualStyleBackColor = true;
+            this.btnAddPathOut.Click += new System.EventHandler(this.btnAddPathOut_Click);
+            // 
+            // lblPathOut
+            // 
+            this.lblPathOut.AutoSize = true;
+            this.lblPathOut.Location = new System.Drawing.Point(12, 41);
+            this.lblPathOut.Name = "lblPathOut";
+            this.lblPathOut.Size = new System.Drawing.Size(120, 13);
+            this.lblPathOut.TabIndex = 3;
+            this.lblPathOut.Text = "Конечная директория:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(433, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(81, 29);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // ExcelToBitmap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 99);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPathOut);
+            this.Controls.Add(this.btnAddPathOut);
+            this.Controls.Add(this.lblPathOut);
+            this.Controls.Add(this.txtExcel);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.lblExcel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ExcelToBitmap";
+            this.Text = "Excel To Bitmap (ilel@list.ru)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblExcel;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.TextBox txtExcel;
+        private System.Windows.Forms.TextBox txtPathOut;
+        private System.Windows.Forms.Button btnAddPathOut;
+        private System.Windows.Forms.Label lblPathOut;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ ExcelToBitmap/ExcelToBitmap.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelToBitmap
+{
+    public partial class ExcelToBitmap : Form
+    {
+        private static string pathExcel;
+        private static string pathOut;
+        
+        private string pathDVD;
+        private string pathCD;
+        private string pathBluRay;
+
+        public ExcelToBitmap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fileDialog = new OpenFileDialog() { Filter = "Книга Excel (*.xlsx)|*.xlsx|Книга Excel 97-2003 (*.xls)|*.xls|All Files (*.*)|*.*" } )
+            {
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtExcel.Text = fileDialog.FileName;
+                    pathExcel = fileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnAddPathOut_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog browserDialog = new FolderBrowserDialog())
+            {
+                if (browserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtPathOut.Text = browserDialog.SelectedPath;
+                    pathOut = browserDialog.SelectedPath;
+
+                    pathDVD = $"{pathOut}\\DVD";
+                    pathCD = $"{pathOut}\\CD";
+                    pathBluRay = $"{pathOut}\\Blu-Ray";
+                }
+            }
+        }
+
+        private static OleDbConnection OleDbConnectionExcel { get; set; }
+
+        private static bool ConnectionExcel(string path, string expansion = ".xlsx")
+        {
+            string connectionstring = string.Empty;
+            try
+            {
+                if (expansion == ".xls")
+                {
+                    connectionstring = $"Provider=Microsoft.Jet.OLEDB.4.0; Data Source = '{path}'; Extended Properties=\"Excel 8.0;HDR=YES;\"";
+                }
+
+                if (expansion == ".xlsx")
+                {
+                    connectionstring = $"Provider=Microsoft.ACE.OLEDB.12.0; Data Source = '{path}'; Extended Properties=\"Excel 12.0;HDR=YES;\"";
+                }
+
+                OleDbConnectionExcel = new OleDbConnection(connectionstring);
+                OleDbConnectionExcel.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private string GetTableName(string strPath)
+        {
+            try
+            {
+                Microsoft.Office.Interop.Excel.Application ExcelObj = new Microsoft.Office.Interop.Excel.Application();
+
+                Microsoft.Office.Interop.Excel.Workbook theWorkbook = null;
+
+                theWorkbook = ExcelObj.Workbooks.Open($"{strPath}", Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                Microsoft.Office.Interop.Excel.Sheets sheets = theWorkbook.Worksheets;
+
+                Microsoft.Office.Interop.Excel.Worksheet worksheet = (Microsoft.Office.Interop.Excel.Worksheet)sheets.get_Item(1);
+
+                var worksheetName = worksheet.Name;
+
+                theWorkbook.Close(true);
+                ExcelObj.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(ExcelObj);
+
+                if (string.IsNullOrWhiteSpace(worksheetName))
+                {
+                    return default;
+                }
+
+                return worksheetName;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }       
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(pathExcel))
+            {
+                MessageBox.Show("Укажите путь к Excel файлу");
+                txtExcel.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(pathOut))
+            {
+                MessageBox.Show("Укажите путь к конечной директории");
+                txtPathOut.Focus();
+                return;
+            }
+
+            if (!Directory.Exists(pathOut))
+            {
+                Directory.CreateDirectory(pathOut);                
+            }
+
+            if (!Directory.Exists(pathDVD))
+            {
+                Directory.CreateDirectory(pathDVD);
+            }
+
+            if (!Directory.Exists(pathCD))
+            {
+                Directory.CreateDirectory(pathCD);
+            }
+
+            if (!Directory.Exists(pathBluRay))
+            {
+                Directory.CreateDirectory(pathBluRay);
+            }
+
+
+            if (!ConnectionExcel(pathExcel))
+            {
+                return;
+            }
+
+            Task.Run(() => ImportExcelKazna(GetTableName(pathExcel)));
+        }
+
+        private async void ImportExcelKazna(string tableName)
+        {
+            if (string.IsNullOrWhiteSpace(tableName))
+            {
+                return;
+            }
+
+            var sql = $"SELECT * FROM [{tableName}$]";
+            var count = $"SELECT COUNT(*) FROM [{tableName}$]";
+
+            using (var command = new OleDbCommand { Connection = OleDbConnectionExcel, CommandText = sql })
+            {
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    var countCD = 0;
+                    var countDVD = 0;
+                    var countBluRay = 0;
+
+                    while (await reader.ReadAsync())
+                    {
+                        if (reader[5].ToString().Contains("CD"))
+                        {
+                            Drawing(reader, pathCD);
+                            countCD++;
+                        }
+                        else if (reader[5].ToString().Contains("DVD"))
+                        {
+                            Drawing(reader, pathDVD);
+                            countDVD++;
+                        }
+                        else if (reader[5].ToString().Contains("Blu-ray"))
+                        {
+                            Drawing(reader, pathBluRay);
+                            countBluRay++;
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show(String.Format("Импорт окончен!"), "Окончание импорта", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void Drawing(DbDataReader reader, string path)
+        {
+            using (Bitmap bitmap = new Bitmap(3189, 2185))
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.Clear(Color.Black);
+
+                    graphics.DrawString(
+                        reader[4].ToString(),
+                        new Font("Verdana", (float)10),
+                        new SolidBrush(Color.White), 15, 0);
+                }
+                bitmap.Save($"{path}\\{reader[0]}.JPEG", ImageFormat.Jpeg);
+            }
+        }
+    }
+}
